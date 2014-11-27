a7ab5a2472bd0f855e124bd4e4c20b6371bd2d25
Kenneth Kasajian <kkasajian@netaphor.com>
2014-11-26 17:05:06 -0800

    initial commit

---
+++ AsyncExample/AsyncExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AsyncExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Method1();
+            Console.ReadKey();
+            Method2();
+            Console.ReadKey();
+            Method3();
+            Console.ReadKey();
+        }
+
+        private static void Method1()
+        {
+            foreach (string database in Svc.GetDatabases())
+            {
+                foreach (string table in Svc.GetTables(database))
+                {
+                    foreach (string column in Svc.GetColumns(database, table))
+                    {
+                        Console.WriteLine("Database: {0}, Table: {1}, Column: {2}", database, table, column);
+                    }
+                }
+            }
+        }
+
+        private static void Method2()
+        {
+            Svc.GetDatabasesAsync(databases =>
+            {
+                foreach (string database in databases)
+                {
+                    Svc.GetTablesAsync(database, tables =>
+                    {
+                        foreach (string table in tables)
+                        {
+                            Svc.GetColumnsAsync(database, table, columns =>
+                            {
+                                foreach (string column in columns)
+                                {
+                                    Console.WriteLine("Database: {0}, Table: {1}, Column: {2}", database, table, column);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+
+        private static async void Method3()
+        {
+            foreach (string database in await Svc.GetDatabasesAsync())
+            {
+                foreach (string table in await Svc.GetTablesAsync(database))
+                {
+                    foreach (string column in await Svc.GetColumnsAsync(database, table))
+                    {
+                        Console.WriteLine("Database: {0}, Table: {1}, Column: {2}", database, table, column);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AsyncExample/AsyncExample/Svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AsyncExample
+{
+    internal class Svc
+    {
+        public static List<string> GetDatabases()
+        {
+            return new List<string> {"db1", "db2"};
+        }
+
+        public static List<string> GetTables(string database)
+        {
+            return new List<string> {"t1", "t2", "t3"};
+        }
+
+        public static List<string> GetColumns(string database, string table)
+        {
+            return new List<string> {"c1", "c2", "c3"};
+        }
+
+        public static void GetDatabasesAsync(Action<List<string>> callback)
+        {
+            new Timer(200) {AutoReset = false, Enabled = true}.Elapsed +=
+                (sender, args) => callback(new List<string> {"db1", "db2"});
+        }
+
+        public static void GetTablesAsync(string database, Action<List<string>> callback)
+        {
+            new Timer(200) {AutoReset = false, Enabled = true}.Elapsed +=
+                (sender, args) => callback(new List<string> {"t1", "t2", "t3"});
+        }
+
+        public static void GetColumnsAsync(string database, string table, Action<List<string>> callback)
+        {
+            new Timer(200) {AutoReset = false, Enabled = true}.Elapsed +=
+                (sender, args) => callback(new List<string> {"c1", "c2", "c3"});
+        }
+
+        public static async Task<List<string>> GetColumnsAsync(string database, string table)
+        {
+            var tsc = new TaskCompletionSource<List<string>>();
+            GetColumnsAsync(database, table, tsc.SetResult);
+            await tsc.Task;
+            return tsc.Task.Result;
+        }
+
+        public static async Task<List<string>> GetTablesAsync(string database)
+        {
+            var tsc = new TaskCompletionSource<List<string>>();
+            GetTablesAsync(database, tsc.SetResult);
+            await tsc.Task;
+            return tsc.Task.Result;
+        }
+
+        public static async Task<List<string>> GetDatabasesAsync()
+        {
+            var tsc = new TaskCompletionSource<List<string>>();
+            GetDatabasesAsync(tsc.SetResult);
+            await tsc.Task;
+            return tsc.Task.Result;
+        }
+    }
+}
