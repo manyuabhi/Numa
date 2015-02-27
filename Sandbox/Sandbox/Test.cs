5fb7239f325b603c25b9182538b9838a52d5a02c
kasajian <ken@kasajian.com>
2015-02-27 09:31:49 -0800

    .

---
+++ Sandbox/Sandbox/Test.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Sandbox
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void Compsose_Test ()
+		{
+			var Z1 = FF.Compose (GG);
+			var a1 = Z1 (3.14);
+			Assert.AreEqual ("3", a1);
+		}
+
+		readonly Func<int, string> FF = i => i.ToString().ToUpper(); 
+		readonly Func<double, int> GG = d => (int) Math.Floor(d);
+	}
+
+	public static class FunctionalExtensions
+	{
+		public static Func<T, V> Compose<T, U, V>(this Func<U, V> f, Func<T, U> g)
+		{
+			return x => f(g(x));
+		}
+	}
+}
+
