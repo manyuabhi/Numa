fa8e2248bed826006118a5786063f1b6c4463f38
Kenneth Kasajian <ken@kasajian.com>
2015-06-05 09:33:22 -0700

    .

--- Sandbox/Sandbox/Test.cs
+++ Sandbox/Sandbox/Test.cs
@@ -9,11 +9,17 @@ namespace Sandbox
 	public class Test
 	{
 		[Test]
-		public void Compsose_Test ()
+		public void Compsose_Test1 ()
 		{
 			var Z1 = FF.Compose (GG);
 			var a1 = Z1 (3.14);
 			Assert.AreEqual ("3", a1);
+		}
+
+		[Test]
+		public void Compsose_Test2 ()
+		{
+
 		}
 
 		readonly Func<int, string> FF = i => i.ToString().ToUpper(); 
@@ -25,6 +31,16 @@ namespace Sandbox
 		public static Func<T, V> Compose<T, U, V>(this Func<U, V> f, Func<T, U> g)
 		{
 			return x => f(g(x));
+		}
+
+		public static Func<T, M<V>> Compose<T, U, V>(this Func<U, M<V>> f, Func<T, M<U>> g)
+		{
+			return x => Bind(g(x), f);
+		}
+
+		public static T Identity<T>(this T value)
+		{
+			return value;
 		}
 	}
 }
